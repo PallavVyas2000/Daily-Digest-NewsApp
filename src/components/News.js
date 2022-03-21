500dcf1058e344b2b4a97179373191c31f62ceed
Pallav Vyas <pallavvyas2000@gmail.com>
2022-03-22 05:11:52 +0530

    Working ( Page Number )

--- src/components/News.js
+++ src/components/News.js
@@ -7,20 +7,22 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
+      pageSize: 21
     }
   }
 
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=1";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=1&pageSize=21";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData)
-    this.setState({ articles: parsedData.articles })
+    this.setState({ articles: parsedData.articles,
+      totalResults: parsedData.totalResults })
   }
 
   handleNextClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=${this.state.page + 1}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=${this.state.page + 1}&pageSize=21`;
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData)
@@ -32,7 +34,7 @@ export class News extends Component {
   }
 
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=${this.state.page - 1}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=${this.state.page - 1}&pageSize=21`;
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData)
@@ -47,10 +49,10 @@ export class News extends Component {
       <>
         <div className="container my-3">
           <h2>Daily Digest - Top Headlines</h2>
-          <div className="row my-4">
+          <div className="row my-4 d-flex justify-content-center">
             {this.state.articles.map((element) => {
               return <div className="col-md-4 my-3" key={element.url}>
-                <NewsItem title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 100):""} newsUrl={element.url} imgUrl={element.urlToImage?element.urlToImage:"https://exchange4media.gumlet.io/news-photo/1522981614_f9REAd_online_content.jpg?format=webp&w=750&dpr=1.0"} />
+                <NewsItem title={element.title?element.title.slice(0, 70):""} description={element.description?element.description.slice(0, 100):""} newsUrl={element.url} imgUrl={element.urlToImage?element.urlToImage:"https://exchange4media.gumlet.io/news-photo/1522981614_f9REAd_online_content.jpg?format=webp&w=750&dpr=1.0"} />
               </div>
             })
             }
@@ -58,7 +60,8 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between">
           <button disabled={this.state.page <= 1} type="button" className="btn btn-dark mx-2 my-2" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button type="button" className="btn btn-dark mx-2 my-2" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button type="button" class="btn btn-dark btn-circle btn-xl">{this.state.page}</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.state.pageSize)} type="button" className="btn btn-dark mx-2 my-2" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </>
     )
