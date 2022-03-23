bba00c56640a4d8f0c867249d3324528d7ae73f2
Pallav Vyas <pallavvyas2000@gmail.com>
2022-03-24 01:27:38 +0530

    Working Infinite Scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,52 +5,52 @@ import { Link } from "react-router-dom";
 export class Navbar extends Component {
   render() {
     return (
-      <div>
+      <div className="navbar-main-body">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">
               <img src={logo} alt="" width="40" height="40" className="d-inline-block align-text-center mx-2" />
               <strong>Daily Digest</strong>
             </Link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="/">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/">
                     Home
                   </Link>
                 </li>
-                <li class="nav-item">
+                <li className="nav-item">
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="/categories/business">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/categories/business">
                     Business
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="/categories/entertainment">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/categories/entertainment">
                     Entertainment
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="/categories/health">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/categories/health">
                     Health
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="/categories/science">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/categories/science">
                     Science
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="/categories/sports">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/categories/sports">
                     Sports
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="/categories/technology">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/categories/technology">
                     Technology
                   </Link>
                 </li>
--- src/components/News.js
+++ src/components/News.js
@@ -1,99 +1,81 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import NewsItem from './NewsItem'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export class News extends Component {
   static defaultProps = {
-      pageSize : 21,
-      country : "in",
-      category : ""
+    pageSize: 21,
+    country: "in",
+    category: ""
   }
 
   static propTypes = {
-      pageSize : PropTypes.number,
-      country : PropTypes.string,
-      category : PropTypes.string
+    pageSize: PropTypes.number,
+    country: PropTypes.string,
+    category: PropTypes.string
   }
-  
+
   capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  fetchMoreData = async () => {
+    this.setState({page: this.state.page + 1})
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults
+    })
   }
 
   constructor(props) {
     super(props);
     this.state = {
       articles: [],
-      loading: false,
       page: 1,
+      totalResults: 0
     }
-    if(this.props.category === "general")
-    {
+    if (this.props.category === "general") {
       document.title = "Top Headlines - Daily Digest";
     }
-    else{
+    else {
       document.title = `${this.capitalizeFirstLetter(this.props.category)} News - Daily Digest`;
     }
   }
 
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({loading : true})
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({loading : false})
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults
     })
   }
-
-  handleNextClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading : true})
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({loading : false})
-    this.setState({
-      page: this.state.page + 1,
-      articles: parsedData.articles
-    })
-
-  }
-
-  handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=78bbc3c429fa4477a22b5c4bf40b05d5&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading : true})
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({loading : false})
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles
-    })
-
-  }
   render() {
     return (
       <>
-        <div className="container my-3">
-          <h1 className="heading text-center">Daily Digest - Top Headlines</h1>
-          {this.state.loading && <div class="d-flex justify-content-center my-auto"> <div className="spinner-border text-center" style={{width: "3rem", height: "3rem"}} role="status">
-            <span className="visually-hidden">Loading...</span>
-          </div></div>}
-          <div className="row my-4 d-flex justify-content-center">
-            {!this.state.loading && this.state.articles.map((element) => {
-              return <div className="col-md-4 my-3" key={element.url}>
-                <NewsItem title={element.title ? element.title.slice(0, 70) : ""} source={element.source.name} author={element.author?element.author:"Unknown"} publishedAt={element.publishedAt} description={element.description ? element.description.slice(0, 100) : ""} newsUrl={element.url} imgUrl={element.urlToImage ? element.urlToImage : "https://exchange4media.gumlet.io/news-photo/1522981614_f9REAd_online_content.jpg?format=webp&w=750&dpr=1.0"} />
-              </div>
-            })
-            }
+        <h1 className="heading text-center">Daily Digest - Top Headlines</h1>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<div className="d-flex justify-content-center my-auto"> <div className="spinner-border text-center" style={{ width: "3rem", height: "3rem" }} role="status">
+          </div></div>}>
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return <div className="col-md-4 my-3" key={element.url}>
+                  <NewsItem title={element.title ? element.title.slice(0, 70) : ""} source={element.source.name} author={element.author ? element.author : "Unknown"} publishedAt={element.publishedAt} description={element.description ? element.description.slice(0, 100) : ""} newsUrl={element.url} imgUrl={element.urlToImage ? element.urlToImage : "https://exchange4media.gumlet.io/news-photo/1522981614_f9REAd_online_content.jpg?format=webp&w=750&dpr=1.0"} />
+                </div>
+              })
+              }
+            </div>
           </div>
-        </div>
-        <div className="container d-flex justify-content-between mt-5 mb-5">
-          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark mx-2 my-2" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button type="button" className="btn btn-dark btn-circle btn-xl">{this.state.page}</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark mx-2 my-2" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
+        </InfiniteScroll>
       </>
     )
   }
