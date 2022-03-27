e701ea97ba888f853f20a408119457137ef0fadf
Pallav Vyas <pallavvyas2000@gmail.com>
2022-03-27 19:02:04 +0530

    Updates

---
+++ precache-manifest.dfdb3c65e478d0d445a0ec44eec5a36c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec957cb25077db3f3e97",
+    "url": "/Daily-Digest-NewsApp/static/css/main.65c57fdf.chunk.css"
+  },
+  {
+    "revision": "ec957cb25077db3f3e97",
+    "url": "/Daily-Digest-NewsApp/static/js/main.ec957cb2.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/Daily-Digest-NewsApp/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "ced2f59dbacdd73f53a6",
+    "url": "/Daily-Digest-NewsApp/static/js/2.ced2f59d.chunk.js"
+  },
+  {
+    "revision": "2fa95fed55cc0b8c8eef",
+    "url": "/Daily-Digest-NewsApp/static/js/runtime~main.2fa95fed.js"
+  },
+  {
+    "revision": "67224f4bb3224d496a412ad51167ee27",
+    "url": "/Daily-Digest-NewsApp/static/media/logo192.67224f4b.png"
+  },
+  {
+    "revision": "a798a4807241b34c483c3af5b90071b1",
+    "url": "/Daily-Digest-NewsApp/index.html"
+  }
+];
