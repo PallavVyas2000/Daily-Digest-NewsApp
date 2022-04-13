4e6a1dcf6eff0ca4d5a9b3e6b212c1441c492adc
Pallav Vyas <pallavvyas2000@gmail.com>
2022-04-13 18:05:26 +0530

    Final Offline 1.1

--- src/App.js
+++ src/App.js
@@ -36,13 +36,13 @@ export default function App(){
           progress={progress}
         />
         <Routes>
-          <Route exact path="/" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="home" pageSize={100} country={"in"} category="general" />} />                                            {/*if you dont add the key element in every route then you would need to manually refresh the page to see new content every time you clink on the nav buttons */}
-          <Route exact path="/categories/technology" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="technology" pageSize={100} country={"in"} category="technology" />} />
-          <Route exact path="/categories/business" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="business" pageSize={100} country={"in"} category="business" />} />
-          <Route exact path="/categories/entertainment" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="entertainment" pageSize={100} country={"in"} category="entertainment" />} />
-          <Route exact path="/categories/health" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="health" pageSize={100} country={"in"} category="health" />} />
-          <Route exact path="/categories/science" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="science" pageSize={100} country={"in"} category="science" />} />
-          <Route exact path="/categories/sports" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="sports" pageSize={100} country={"in"} category="sports" />} />
+          <Route exact path="/" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="home" pageSize={21} country={"in"} category="general" />} />                                            {/*if you dont add the key element in every route then you would need to manually refresh the page to see new content every time you clink on the nav buttons */}
+          <Route exact path="/categories/technology" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="technology" pageSize={21} country={"in"} category="technology" />} />
+          <Route exact path="/categories/business" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="business" pageSize={21} country={"in"} category="business" />} />
+          <Route exact path="/categories/entertainment" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="entertainment" pageSize={21} country={"in"} category="entertainment" />} />
+          <Route exact path="/categories/health" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="health" pageSize={21} country={"in"} category="health" />} />
+          <Route exact path="/categories/science" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="science" pageSize={21} country={"in"} category="science" />} />
+          <Route exact path="/categories/sports" element={<News mode = {mode} toggleMode = {toggleMode} setprogress={setprogress} key="sports" pageSize={21} country={"in"} category="sports" />} />
         </Routes>
       </div>
     </Router>
